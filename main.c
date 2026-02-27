71cf8943165f6dfb92b2977c914db64ab87b56f7
Taha_arch <taha.elatoui.03@gmail.com>
2026-02-27 00:30:28 +0000

    error handling changes

--- main.c
+++ main.c
@@ -14,41 +14,80 @@
 #include <stdio.h>
 #include <unistd.h>
 
+void	process_and_init(t_stack **a, char **av, int ac)
+{
+	char	**split;
+	int		i;
+
+	i = 1;
+	while (i < ac)
+	{
+		split = ft_split(av[i]);
+		if (!split)
+			print_error(a, NULL, false);
+		init_stack(a, split, true);
+		free_av(split);
+		i++;
+	}
+}
+
+char	**parse_args(int ac, char **av, t_stack **a)
+{
+	char	**split;
+
+	if (ac == 2 && av[1][0] == '\0')
+	{
+		write(2, "Error\n", 6);
+		exit(1);
+	}
+	if (ac == 2)
+	{
+		split = ft_split(av[1]);
+		init_stack(a, split, true);
+		return (split);
+	}
+	process_and_init(a, av, ac);
+	return (NULL);
+}
+
+void	do_sort(t_stack **a, t_stack **b)
+{
+	int	size;
+
+	assign_index(*a);
+	size = stack_size(*a);
+	if (size == 2)
+		sort_two(a);
+	else if (size == 3)
+		sort_three(a);
+	else if (size <= 5)
+		sort_five(a, b);
+	else
+		push_swap(a, b);
+}
+
 int main(int ac, char **av)
 {
-    t_stack *a;
-    t_stack *b;
+    t_stack	*a;
+    t_stack	*b;
+    char	**split;
 
     a = NULL;
     b = NULL;
     if (ac < 2)
         return (0);
-    if (ac == 2 && av[1][0] == '\0')
-        return (write(2, "Error\n", 6));
-    else if (ac == 2)
-    {
-        av = ft_split(av[1]);
-        init_stack(&a, av, ac == 2);
-    }
-    else
-        init_stack(&a, av + 1, ac == 2);
+    split = parse_args(ac, av, &a);
     if (is_sorted(a))
     {
         free_list(&a);
+        if (split)
+            free_av(split);
         return (0);
     }
-    assign_index(a);
-    int size = stack_size(a);
-    if (size == 2)
-        sort_two(&a);
-    else if (size == 3)
-        sort_three(&a);
-    else if (size <= 5)
-        sort_five(&a, &b);
-    else
-        push_swap(&a, &b);
-
+    do_sort(&a, &b);
     free_list(&a);
     free_list(&b);
+    if (split)
+        free_av(split);
     return (0);
 }
